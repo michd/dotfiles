8db7a9858d20aa3886d6fcf229ba9db555cd848d
Michael Duerinckx <duerinckxmichael@gmail.com>
2020-06-21 23:46:20 +0100

    Configure all the things: script it up
    This adds a lot of nonsense I spent way too much time on:
    
    - Split up i3 config files, with a bash script to assembly them,
      config.d/ style
      - More on that split up: all the colors used in i3 config are now set
        with variables, since that's a feature in i3's config, nice. With
        this concatenation approach, I can easily swap out color scheme
        files
    - More on those color schemes: I like picking a hue and then varying
      saturation and lightness for various elements to make a color scheme
      for i3's mininmal chrome. That was a total pain so I made a python
      script for it that works from a "recipe" that includes hue,
      saturation, lightness adjustments with respect to a base hex color.
      Then, each variable has a set of these adjustments. The script just
      takes a hex color and goes from there, spits out a color scheme file
      to work with the rest of the config
    - Of course that python script can also derive the adjustments needed
      from an _existing_ color scheme and a base hex color, because I wasn't
      going to calculate all those values manually from my existing scheme
    - A new gold color scheme is now default. I'm not happy with the
      adjustment values yet and the color scheme generator thing can use
      more work to take a hue, max its saturation, set the lightness to 50%
      or something, and then derive everything else from there. I'm
      rambling.
    - I completely revamped the install.sh script so it can be used in most
      cases, interactively. It'll check for conflicts and ask what to do
      with it, so it's less of a chore to get set up with these dotfiles. It
      also asks to skip some optional stuff, so it's more adaptable to
      multiple sorts of environments.
    
    TODO: take the colors thing further and include .dunstrc for
    notification colors. Those are still hardcoded, and that file doesn't
    support variables. So I'm thinking the file should come from a template
    and have a generator script that'll subtitute in the actual color
    values. That would mean I'd have to pull my color scheme stuff out of
    the i3 config directory, however.
    
    This commit message is now long enough. Happy summer solstice!

---
+++ .config/i3/config.d/available/colorgen.py
@@ -0,0 +1,206 @@
+#!/bin/python3
+
+import colorsys
+import sys
+
+# Color scheme formula:
+# dictionary of all the keys found in the color.conf file, with as value
+# a tuple of hue, saturation, and value adjustments
+
+recipe1 = {
+    "dmenu color": {
+        "c_m_normal_bg": (0, -0.254, -0.239),
+        "c_m_normal_fg": (0, -0.594, 0.467),
+        "c_m_selected_bg": (0, -0.246, 0.165),
+        "c_m_selected_fg": (-0.734, -0.641, 0.639),
+    },
+
+    "window colors": {
+        "c_c_focus_active_border": (0, 0.000, 0.000),
+        "c_c_focus_active_bg": (0, 0.000, 0.000),
+        "c_c_focus_active_text": (-0, -0.594, 0.467),
+        "c_c_focus_active_indic": (-0, -0.246, 0.165),
+
+        "c_c_focus_inactive_border": (0, -0.475, 0.086),
+        "c_c_focus_inactive_bg": (0, -0.467, 0.000),
+        "c_c_focus_inactive_text": (0, -0.559, 0.212),
+        "c_c_focus_inactive_indic": (0, -0.559, 0.212),
+
+        "c_c_focused_border": (0, -0.475, 0.086),
+        "c_c_focused_bg": (0, -0.467, 0.000),
+        "c_c_focused_text": (0, -0.559, 0.212),
+        "c_c_focused_indic": (0, -0.559, 0.212),
+
+        "c_c_urgent_border": (-0.585, 0.062, 0.169),
+        "c_c_urgent_bg": (-0.585, 0.062, 0.169),
+        "c_c_urgent_text": (-0.581, -0.590, 0.639),
+        "c_c_urgent_indic": (-0.585, -0.205, 0.412),
+
+        "c_c_placeholder_border": (-0.734, -0.641, -0.361),
+        "c_c_placeholder_bg": (-0.734, -0.641, -0.314),
+        "c_c_placeholder_text": (-0.734, -0.641, 0.639),
+        "c_c_placeholder_indic": (-0.734, -0.641, -0.361),
+
+        "c_c_bg": (0, 0.287, -0.196),
+    },
+
+    "bar colors": {
+        "c_b_bg": (0, -0.4, -0.6),
+        "c_b_status_text": (0, -1, 0.8),
+        "c_b_separator": (0, 0.000, 0.000),
+
+        "c_b_focused_ws_border": (0, -0.246, 0.165),
+        "c_b_focused_ws_bg": (0, 0.000, 0.000),
+        "c_b_focused_ws_text": (0, -0.641, 0.6),
+
+        "c_b_active_ws_border": (0, 0, 0),
+        "c_b_active_ws_bg": (0, 0, 0),
+        "c_b_active_ws_text": (0, -0.594, 0.6),
+
+        "c_b_inactive_ws_border": (0, -0.269, -0.161),
+        "c_b_inactive_ws_bg": (0, -0.254, -0.239),
+        "c_b_inactive_ws_text": (0, -0.573, 0.3),
+
+        "c_b_urgent_ws_border": (-0.585, 0.062, 0.169),
+        "c_b_urgent_ws_bg": (-0.585, 0.062, 0.169),
+        "c_b_urgent_ws_text": (-0.581, -0.590, 0.639),
+
+        "c_b_binding_mode_border": (-0.585, 0.062, 0.169),
+        "c_b_binding_mode_bg": (-0.585, 0.062, 0.169),
+        "c_b_binding_mode_text": (-0.581, -0.590, 0.639),
+    }
+}
+
+def main():
+    if len(sys.argv) < 2:
+        print("Please provide a hex color to start from.")
+        exit(1)
+
+    base_color = sys.argv[1]
+
+    # Trim leading '#'
+    if base_color[0] == '#': base_color = base_color[1:]
+
+    try:
+        hex_to_rgb(base_color)
+    except ValueError:
+        print(f"Invalid base hex color #{base_color}")
+        exit(1)
+
+    if len(sys.argv) == 2:
+        scheme = generate_scheme(base_color, recipe1)
+        for l in scheme: print(l)
+
+    elif sys.argv[2] == "reverse":
+        if len(sys.argv) < 4:
+            print("Please provide a filename with a colorscheme to reverse")
+            exit(1)
+
+        filename = sys.argv[3]
+        reverse_scheme = reverse_engineer_scheme(base_color, filename)
+        for l in reverse_scheme: print(l)
+
+def get_longest_color_name(recipe):
+    longest_name_len = 1
+    for r_name, s_recipe in recipe.items():
+        local_longest = max([len(n) for n in s_recipe.keys()])
+        if local_longest > longest_name_len:
+            longest_name_len = local_longest
+    return longest_name_len
+
+
+def generate_scheme(base_hex_color, recipe):
+    file_header = "# Colors "
+    line_length = 79
+
+    longest_name_len = get_longest_color_name(recipe)
+
+    scheme_lines = [
+            f"{file_header:-<{line_length}}"
+        ]
+
+    for section_heading, sub_recipe in recipe.items():
+        scheme_lines.append(f"# {section_heading}")
+        scheme_lines.extend(
+                generate_sub_scheme(
+                    base_hex_color, sub_recipe, longest_name_len))
+        scheme_lines.append("")
+
+    return scheme_lines
+
+def generate_sub_scheme(base_hex_color, sub_recipe, longest_name_len):
+    base_hsv = hex_to_hsv(base_hex_color)
+
+    scheme_list = []
+
+    for color_name, adjustments in sub_recipe.items():
+        new_color = hsv_to_hex(*adjust_hsv(*base_hsv, *adjustments))
+        scheme_list.append(
+                "set " \
+                f"${color_name: <{longest_name_len}} "
+                f"{new_color}")
+
+    return scheme_list
+
+def reverse_engineer_scheme(base_hex_color, filename):
+    lines = []
+    with open(filename) as f: lines = f.readlines()
+    lines = [l.strip() for l in lines]
+
+    base_h, base_s, base_v = hex_to_hsv(base_hex_color)
+
+    named_hex_colors = {}
+    adjustments = []
+
+    for l in lines:
+        if not l.startswith("set $"): continue
+        name = l[5:-6].strip()
+        hex_color = l[-6:]
+        named_hex_colors[name] = hex_color
+
+    for n, hex_color in named_hex_colors.items():
+        h, s, v = hex_to_hsv(hex_color)
+        adjustments.append(
+                f"\"${n}\": ("
+                f"{(h - base_h):.3f}, "
+                f"{(s - base_s):.3f}, "
+                f"{(v - base_v):.3f}),")
+ 
+
+    return adjustments
+
+def adjust_hsv(h, s, v, h_adj, s_adj, v_adj):
+    h += h_adj
+    # If it's over or under, wrap around
+    h = h if h >= 0 else 1 - (abs(h) % 1)
+    h = h if h <= 1 else h % 1
+
+    s += s_adj
+    # Clip to 0..1.0
+    s = s if s >= 0 else 0
+    s = s if s <= 1.0 else 1.0
+
+    v += v_adj
+    # Clip to 0..1.0
+    v = v if v >= 0 else 0
+    v = v if v <= 1.0 else 1.0
+
+    return h, s, v
+
+def hex_to_rgb(hex_color):
+    return int(hex_color[0:2], 16) / 255.0,\
+           int(hex_color[2:4], 16) / 255.0,\
+           int(hex_color[4:6], 16) / 255.0
+
+def rgb_to_hex(r, g, b):
+    return f"{hex(int(r * 255))[2:].upper():0>2}" \
+            f"{hex(int(g * 255))[2:].upper():0>2}" \
+            f"{hex(int(b * 255))[2:].upper():0>2}"
+
+def hex_to_hsv(hex_color):
+    return colorsys.rgb_to_hsv(*hex_to_rgb(hex_color))
+
+def hsv_to_hex(h, s, v):
+    return rgb_to_hex(*colorsys.hsv_to_rgb(h, s, v))
+
+main()
--- install.sh
+++ install.sh
@@ -1,16 +1,139 @@
 #!/bin/bash
 
 dotfiles="$(pwd)/"
-pushd $HOME
-ln -s "$dotfiles"".gitconfig" ./
-ln -s "$dotfiles"".vimrc" ./
-ln -s "$dotfiles"".bash_profile" ./
-ln -s "$dotfiles"".tmux.conf" ./
-ln -s "$dotfiles"".vim/" ./
-ln -s "$dotfiles"".irbrc" ./
-ln -s "$dotfiles"".gemrc" ./
-ln -s "$dotfiles"".config" ./
+destination="$HOME/"
 
-echo "source ~/.bash_profile" >> .bashrc
-popd
-echo "Dotfiles installed."
+main() {
+  pushd $destination
+
+  make_symlink ".gitconfig"
+  make_symlink ".vimrc"
+  make_symlink ".bash_profile"
+  make_symlink ".tmux.conf"
+  make_symlink ".vim"
+
+  read -p "Install .config folder? (Y/n): " config_yn
+  if [ $(is_yes "$config_yn") = true ]; then
+    make_symlink ".config"
+
+    echo "Would you like to assemble an i3 config now?"
+    echo "If not, go to ~/.config/i3 and run ./make-i3-config later."
+    read -p "Make i3 config? (Y/n): " conf_yn
+
+    if [ $(is_yes "$conf_yn") = true ]; then
+      pushd ".config/i3"
+      ./make-i3-config
+      popd
+    else
+      "Skipped making i3 config file"
+    fi
+  else
+    echo "Skipped .config folder."
+  fi
+
+  read -p "Install ruby-related dotfiles? (Y/n): " ruby_yn
+
+  if [ $(is_yes "$ruby_yn") = true ]; then
+    make_symlink ".irbrc"
+    make_symlink ".gemrc"
+  else
+    echo "Skipped ruby-related dotfiles."
+  fi
+
+  echo "source ~/.bash_profile" >> .bashrc
+
+  popd
+  echo "Dotfiles installed."
+}
+
+make_symlink() {
+  local symlinkName=$1
+  local originPath="$dotfiles""$1"
+  local destPath="./$1"
+
+  if [ -L "$destPath" ]; then
+    # A symlink already exists like this
+
+    if [ "$(readlink $destPath)" -ef "$originPath" ]; then
+      # It points at the thing we were going to point it at anyway, so never
+      # mind.
+      echo "$destPath was already symlinked, skipping."
+    else
+      resolve_file_conflict $symlinkName
+    fi
+
+  elif [ -d "$destPath" ]; then
+    # A directory with this name already exists
+
+    resolve_conflict $1 "folder"
+
+  elif [ -f "$destPath" ]; then
+    # Destination file already exists, ask what to do
+
+    resolve_conflict $1 "file"
+
+  else
+    # No conflicting existing file
+
+    ln -s "$originPath" ./
+    echo "Installed symlink $destPath"
+
+  fi
+}
+
+resolve_conflict() {
+  local symlinkName=$1
+  local type=$2
+  local originPath="$dotfiles""$1"
+  local destPath="./$1"
+
+  echo "$type $destPath already exists. What would you like to do?"
+  echo "(O)verwrite, renaming the $type as backup, (K)eep existing $type,"
+  echo "(A)bort further installation"
+  read -p "Default = Overwrite; your choice: " conflict_a
+
+  if [ -z "$conflict_a" ] || [ "o" = "$conflict_a" ] \
+    || [ "O" = "$conflict_a" ]; then
+
+    # Overwrite option
+    echo "Backing up existing $type $destPath as $destPath.bak"
+    mv "$destPath" "$destPath.bak"
+    echo "Adding symlink for $destPath"
+    ln -s "$originPath" ./
+
+    if [ "$type" = "folder" ]; then
+      echo "
+IMPORTANT: since an existing folder was moved, please manually ensure any of
+its contents are copied or moved to the appropriate place."
+    fi
+
+  elif [ "k" = "$conflict_a" ] || [ "K" = "$conflict_a" ]; then
+
+    # Keep option
+    echo "Keeping existing $type $destPath as-is."
+
+  elif [ "a" = "$conflict_a" ] || [ "A" = "$conflict_a" ]; then
+
+    # Abort option
+    echo "Aborting further installation, $destPath kept as-is."
+    exit 1
+
+  else
+
+    # User didn't enter anything sensible
+    echo "Invalid input; keeping existing $type $destPath as-is."
+
+  fi
+}
+
+is_yes() {
+  if [ -z "$1" ] || [ "y" = "$1" ] || [ "Y" = "$1" ]; then
+    echo true
+  else
+    echo false
+  fi
+}
+
+# Invoke main function
+main
+
