b8e6aa66c68c4d6dffe86be17591a986913e0394
Michael Duerinckx <mich@michd.me>
2026-05-10 16:00:17 +0100

    wm/bar/kbstatus: independent of X11

--- .config/wm/bar/available/kbstatus.sh
+++ .config/wm/bar/available/kbstatus.sh
@@ -1,24 +1,12 @@
 #!/bin/bash
 
-capsLockSearch="Caps\s*Lock:\s*on"
-numLockSearch="Num\s*Lock:\s*on"
-
 is_on() {
-  if [ -z "$(xset -q | grep $1)" ]; then
-    echo false
-  else
-    echo true
-  fi
+  [ "$(cat /sys/class/leds/input*::$1/brightness | head -n1)" = "1" ]
 }
 
-if [ "$(is_on $numLockSearch)" = true ]; then
-  echo -n "[1]"
-fi
+if is_on numlock; then echo -n "[1]"; fi
 
-if [ "$(is_on $capsLockSearch)" = true ]; then
-  if [ "$(is_on $numLockSearch)" = true ]; then
-    echo -n " "
-  fi
+if is_on capslock; then
+  if is_on numlock; then; echo -n " "; fi
   echo -n "[A]"
 fi
-
